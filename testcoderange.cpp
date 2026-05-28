70410e1532968e752aae2dcd78c9302f4e821a71
Ricky Vo <124307637+FluffyUnicorn23@users.noreply.github.com>
2026-05-27 19:37:28 -0700

    Implement PID control for servo
    Added PID control for servo angle adjustment based on distance readings.

--- testcoderange.cpp
+++ testcoderange.cpp
@@ -7,6 +7,9 @@
 // Define the pin you connected the servo signal wire to
 const uint SERVO_PIN = 15;
 
+#define constrain(val, low, high) \
+    ((val) < (low) ? (low) : ((val) > (high) ? (high) : (val)))
+
 void setup_servo() {
     gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
@@ -29,7 +32,6 @@ void set_servo_angle(uint slice_num, float angle) {
 
 int main() {
     stdio_init_all();
-
     // I2C init (example for Pico 2W - adjust pins if needed)
     i2c_init(i2c0, 400 * 1000);
     gpio_set_function(16, GPIO_FUNC_I2C);
@@ -48,22 +50,51 @@ int main() {
     setup_servo();
     uint slice = pwm_gpio_to_slice_num(SERVO_PIN);
 
+    float previous_error = 0.0; 
+    float integral = 0.0;
+    float target = 3.0; // in inches
+    float Kp = 0.5; // most sensititve factor, change this maybe statically 
+    float Ki = 0;
+    float Kd = 0;
+    
+    float DT = 0.025;
+    float error = 0.0;
+    float angle = 90.0;
+    set_servo_angle(slice, angle);
+    int count = 0;
+
+    int c = getchar();
+    if (c == 'c') {
+            
+
     while (1) {
-        uint16_t dist_int = sensor.readRangeContinuousMillimeters();
-        float dist = (dist_int / 25.4f) - 2.0f;
+        uint16_t dist_int = sensor.readRangeContinuousMillimeters(); // stop reading correctly at around an inch
+        float dist = (dist_int / 25.40f) - 1.63f; // change minus value for callibration, the higher the number the lower the number
         printf("Distance: %.2f inches\n", dist);
-        sleep_ms(50);
-        if(dist < 2.0){
-            set_servo_angle(slice, -30);
-            sleep_ms(100);
-        }
-        else if (dist < 3.0) {
-            set_servo_angle(slice, 60);
-            sleep_ms(100);
-        }
-        else {
-            set_servo_angle(slice, 150);
-            sleep_ms(100);
-        }
-    }
-}
+        sleep_ms(25);
+        error = target - dist;
+        integral += error * DT;
+        float derivative = (error - previous_error) / DT;
+        float output = Kp*error + Kd*derivative + Ki*integral;
+        //output = constrain(output, -6.5f, 6.5f);
+        printf("output: %.2f \n", output);
+        angle =  90 - output * 10.0;
+      
+        printf("Kp: %.2f, Kd: %.2f, Ki: %.2f, Angle: %.2f \n", Kp, Kd, Ki, angle);
+        previous_error = error;
+        
+        // if (abs(error) > 0.5 && Kp < 0.8) {
+        //     Kp += 0.05;
+        //     }
+        //     else if (abs(error) < 0.2 && Kp > 0.3) {
+        //         Kp -= 0.05;
+        //     }
+            Ki = Kp * 0.1;
+            Kd = Kp * 0.3;
+            // ++count;
+            set_servo_angle(slice, angle);
+        }      
+
+        
+     }   
+}   
